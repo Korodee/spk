8e68c4ddbcf276f524bfbbc944125e70b02c180a
Korodee <koroskki@gmail.com>
2025-07-03 16:04:40 -0700

    Err fix

--- src/components/VipSection.tsx
+++ src/components/VipSection.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { activities } from "@/lib/activities";
-import { FiStar } from "react-icons/fi";
 
 const VipSection = () => {
   const vipActivity = activities.find((a) => a.slug === "salle-arcade-vip");
